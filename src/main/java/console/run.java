81e2e62631d7e2c11d7a2ffb639255dab2c71e65
unknown <2036150446@qq.com>
2021-09-09 01:42:57 +0800

    init

---
+++ src/main/java/console/run.java
@@ -0,0 +1,48 @@
+package console;
+
+import org.jline.reader.*;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import org.zeroturnaround.exec.ProcessExecutor;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.TimeoutException;
+
+public class run {
+    public static void main(String[] args) throws IOException, InterruptedException, TimeoutException {
+        Terminal terminal = TerminalBuilder.builder()
+                .system(false)
+                .streams(System.in, System.out)
+                .build();
+        LineReader lineReader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .build();
+        HashMap<String,String> CmdMap = new utilities().readConfig();
+        String prompt = "PrismConsole> ";
+        System.out.println("PrismConsole--A Shell Executor");
+        System.out.println("Powered by Gundami");
+        new utilities().exec("AutoStart",CmdMap);
+        while (true) {
+            String line;
+            try {
+                line = lineReader.readLine(prompt);
+                if(new utilities().exec(line,CmdMap)&&line!="AutoStart"){
+                    System.out.println("OK");
+                }else {
+                    System.out.println("Error");
+                }
+
+            } catch (UserInterruptException e) {
+                // Do nothing
+            } catch (EndOfFileException e) {
+                System.out.println("\nBye.");
+                return;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/console/utilities.java
@@ -0,0 +1,49 @@
+package console;
+
+import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.stream.slf4j.Slf4jStream;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
+
+public class utilities {
+    ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+    public HashMap<String,String> readConfig() throws IOException {
+        HashMap<String,String> CmdMap = new HashMap<>();
+        Properties config = new Properties();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("config.properties"));
+        config.load(bufferedReader);
+        Enumeration en = config.propertyNames();
+        while (en.hasMoreElements()){
+            String key= (String) en.nextElement();
+            String value=config.getProperty(key);
+            CmdMap.put(key,value);
+        }
+        return CmdMap;
+    }
+    public boolean exec(String cmd,HashMap<String,String> CmdMap) throws IOException, InterruptedException, TimeoutException {
+        if (CmdMap.containsKey(cmd)){
+            String execCmd = CmdMap.get(cmd);
+            cachedThreadPool.execute(() -> {
+                try {
+                    new ProcessExecutor().command(execCmd).redirectOutput(Slf4jStream.of(cmd).asInfo()).execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (TimeoutException e) {
+                    e.printStackTrace();
+                }
+            });
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+}
